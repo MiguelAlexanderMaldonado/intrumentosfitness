ace4570f65a5f639b6413f398c2190b8743a0d0d
Miguel Maldonado <miguelalexandermaldonado@gmail.com>
2022-07-31 18:50:46 +0200

    Actualización script

--- scripts/copia_ficheros_windows.bat
+++ scripts/copia_ficheros_windows.bat
@@ -3,7 +3,8 @@
 	echo off
 	echo Empieza la copia WEB %time%
 	echo -------------------------------------------------------------------------
-	rmdir C:\Users\migue\Documents\GitHub\web
+	del "C:\Users\migue\Documents\GitHub\web\*" /S /F /Q
+	pause
 	xcopy C:\Users\migue\Documents\GitHub\intrumentosfitness C:\Users\migue\Documents\GitHub\web /s /c /k /y
 	echo Copia WEB completada %time%
 	pause
